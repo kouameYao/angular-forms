a5cc731b5e7bf27faee47b18effd75a4e5efd749
kouameYao <yaojean0857@gmail.com>
2021-10-12 10:36:28 +0000

    feat : Passing information from parent-to-child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-forms';
+  nom= '';
+  preNom= '';
+  age : number = 0;
+  asBody= true;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.nom= 'Kouame';
+    this.preNom= 'Yao Jean Junior';
+    this.age= 25;
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+
+  @Input() nom = '';
+  @Input() preNom = '';
+  @Input() age = 0;
+  @Input() asBody= false
 
   constructor() { }
 
