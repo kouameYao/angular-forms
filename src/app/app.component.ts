6c10d5fcdb5fbe78bace3c89d76c51ed6469d6eb
kouameYao <yaojean0857@gmail.com>
2021-10-13 08:25:25 +0000

    forwarded informatioms between component end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,17 @@ export class AppComponent {
   age : number = 0;
   asBody= true;
 
+  items = ['item1', 'item2', 'item3', 'item4'];
+
+  constructor(){}
+
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.nom= 'Kouame';
     this.preNom= 'Yao Jean Junior';
     this.age= 25;
+  }
+
+  addItem(newItem: string) {
+    this.items.push(newItem);
   }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -12,9 +12,15 @@ export class ChildComponent implements OnInit {
   @Input() age = 0;
   @Input() asBody= false
 
+  @Output() newItemEvent = new EventEmitter<string>()
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewItem(value: string) {
+    this.newItemEvent.emit(value);
   }
 
 }
