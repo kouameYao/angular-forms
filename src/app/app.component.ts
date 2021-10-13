c3cacb68844f799a9ad9359c737098f345d66d9d
kouameYao <yaojean0857@gmail.com>
2021-10-13 15:15:20 +0000

    Adding switch button and some stylings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+export type EditorType = 'name' | 'profile';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -24,5 +27,19 @@ export class AppComponent {
 
   addItem(newItem: string) {
     this.items.push(newItem);
+  }
+
+  editor: EditorType = 'name';
+
+  get showNameEditor() {
+    return this.editor === 'name';
+  }
+
+  get showProfileEditor() {
+    return this.editor === 'profile';
+  }
+
+  toggleEditor(type: EditorType) {
+    this.editor = type;
   }
 }
